e17df59bb4cb7f2e246cb1f619cd36039d59e624
MrDmitrij <mr.dima.markevich@mail.ru>
2022-05-18 18:46:32 +0300

    feat: complete 4th task

--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -1,1 +1,32 @@
+const fs = require('fs');
+const path = require('path');
 
+let dirPath = path.join(__dirname, 'files');
+let dirPathCopy = path.join(__dirname, 'files-copy');
+
+fs.mkdir(dirPathCopy, { recursive: true }, (err) => {
+    if (err) throw err;
+});
+
+fs.readdir(dirPathCopy, (err, files) => {
+    if (err) throw err;
+    for (let i = 0; i < files.length; i++) {
+        let copy = path.join(dirPathCopy, files[i]);
+
+        fs.unlink(copy, (err) => {
+            if (err) throw err;
+        });
+    };
+});
+
+fs.readdir(dirPath, (err, files) => {
+    if (err) throw err;
+    for (let i = 0; i < files.length; i++) {
+        let file = path.join(dirPath, files[i]);
+        let copy = path.join(dirPathCopy, files[i]);
+
+        fs.copyFile(file, copy, (err) => {
+            if (err) throw err;
+        });
+    };
+});
