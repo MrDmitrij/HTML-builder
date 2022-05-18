1961b0e3f5ed9fce5e17b471edaa59b44beb0e16
MrDmitrij <mr.dima.markevich@mail.ru>
2022-05-18 17:35:43 +0300

    feat: complete 1st task

---
+++ 01-read-file/index.js
@@ -0,0 +1,9 @@
+const fs = require('fs');
+const path = require('path');
+
+const filePath = path.join(__dirname, 'text.txt');
+let readableStream = fs.createReadStream(filePath, 'utf-8');
+
+readableStream.on('data', function (chunk) {
+    console.log(chunk);
+})
